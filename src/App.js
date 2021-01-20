0a8121f3b16642b3c0d8ab03a3042f8e261f9a60
cooperama <coope133@gmail.com>
2021-01-20 13:56:51 -0800

    Adds Router to project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React from "react";
-import AboutUs from "./pages/AboutUs";
+// Global Styles
 import GlobalStyle from "./components/GlobalStyle";
+// Components
+import AboutUs from "./pages/AboutUs";
+import ContactUs from './pages/ContactUs';
+import OurWork from './pages/OurWork';
 import Nav from './components/Nav';
+// Router
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
