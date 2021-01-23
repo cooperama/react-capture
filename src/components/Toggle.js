a3ca2b10a4e3db78d97bac169c01c84bebc960da
cooperama <coope133@gmail.com>
2021-01-22 17:50:13 -0800

    Adds toggle animation to FaqSection component

---
+++ src/components/Toggle.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import { motion } from 'framer-motion';
+
+export default function Toggle({ title, children}) {
+  const [toggle, setToggle] = useState(true);
+  return (
+    <motion.div layout onClick={() => setToggle(!toggle)}>
+      <motion.h4 layout>{title}</motion.h4>
+      {toggle ? children : ''}
+      <div className="faq-line"></div>
+    </motion.div>
+  )
+}
